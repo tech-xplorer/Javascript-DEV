ff43540c01e5350600fb9cc6ec68e4cd32ed090f
@techxplorer <flxolola@gmail.com>
2019-06-27 20:08:46 +0300

    Advancing with setting up my js development environment

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+let port = 8080;
+let app = express();
+const compiler = webpack(config)
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+})); 
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else{
+    open('http://localhost:' + port);
+  }
+});
