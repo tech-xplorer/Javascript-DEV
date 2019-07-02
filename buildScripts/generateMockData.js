7d46893b2cc4788c19319d720d134af9da228954
@techxplorer <flxolola@gmail.com>
2019-07-02 20:06:26 +0300

    added a mock data generating file

---
+++ buildScripts/generateMockData.js
@@ -0,0 +1,24 @@
+/* This script generate mock data for local development. */
+
+/* eslint-disable no-console */
+
+import jsf from 'json-schema-faker';
+import {schema} from './mockDataSchema';
+import fs from 'fs';
+import chalk from 'chalk';
+import faker from "faker"
+
+// This is a fix from github which is not in the video
+jsf.extend("faker", function() {
+  return faker
+})
+
+const json = JSON.stringify(jsf(schema));
+
+fs.writeFile("./src/api/db.json", json, function (err) {
+  if (err) {
+    return console.log(chalk.red(err));
+  } else {
+    console.log(chalk.green("Mock data generated."));
+  }
+});
