f3e807d2529fcefb5efa504936bf2eaaf09aa16d
@techxplorer <flxolola@gmail.com>
2019-07-02 17:49:00 +0300

    new changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import './index.css';
-import numeral from 'numeral';
-debugger;
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this course!`);
+import {getUsers} from './api/userApi';
+
+// Populate table of users via API call
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody += `<tr>
+    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>
+    </tr>`
+  });
+  window.document.getElementById('users').innerHTML = usersBody;
+});
